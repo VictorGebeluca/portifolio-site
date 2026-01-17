944c6fa215b6e37b9dbc8fe12bd3d34b7e03ea3a
Victor <victormiguel01@gmail.com>
2026-01-17 19:16:50 -0300

    primeiro commit do meu portifolio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-zinc-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-400">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
